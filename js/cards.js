2af730bff64e0f2b6d58e78ad4e9195abf0a1290
dheerajkraaj <dheeraj.btpl@semiconindia.org>
2025-01-06 17:10:36 +0530

    right bottom button added

--- js/cards.js
+++ js/cards.js
@@ -117,3 +117,15 @@ document.addEventListener("DOMContentLoaded", () => {
     showLessBtnSpkr2023.classList.add("d-none");
   });
 });
+
+// $(window).scroll(function() {
+//   if($(this).scrollTop() != 0) {
+//     $('#to-top').fadeIn(); 
+//   } else {
+//     $('#to-top').fadeOut();
+//   }
+// });
+
+// $('#to-top').click(function() {
+//   $('body,html').animate({scrollTop:0},"fast");
+// });
